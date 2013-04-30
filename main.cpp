99c5bdf978519357a5d317592593069caad7d925
Tor E Hagemann <hagemt@rpi.edu>
2013-04-30 02:11:10 -0400

    Why doesn't this work?

--- main.cpp
+++ main.cpp
@@ -1,12 +1,85 @@
 #include <cstdlib>
 #include <cmath>
+#include <utility>
+#include <vector>
+
+#if 1
+#include <tr1/unordered_set>
+#else
+#include <unordered_set>
+#endif
+
 #include "image.h"
 
-// ===================================================================================================
-// ===================================================================================================
+namespace std {
+#if 1
+namespace tr1 {
+#endif
+template<> struct hash<Color> {
+	typedef Color argument_type;
+	typedef std::size_t result_type;
+	result_type operator()(const argument_type &arg) const {
+		result_type val = (result_type) (arg.red ^ arg.green ^ arg.blue);
+		return val;
+	}
+};
+#if 1
+} // namespace tr1
+#endif
+} // namespace std
 
+// ============================================================================
+// ============================================================================
 
-void
+#if 1
+typedef std::tr1::unordered_set<Color> BUCKET;
+#else
+typedef std::unordered_set<Color> BUCKET;
+#endif
+static const Color WHITE(255, 255, 255);
+static const Offset ZERO(0, 0);
+
+static int
+TryCompress(
+		const Image<Color> &input, const Image<Offset> &offset,
+		const int s_offset, const int s_hash,
+		BUCKET *hash, std::pair<int, int> &most)
+{
+	assert(hash);
+	/* Track hits into the offset table */
+	std::vector<std::vector<int> > hits(s_offset, std::vector<int>(s_offset, 0));
+	/* Run the hashing as currently offset */
+	int w = input.Width();
+	int h = input.Height();
+	int mode, collisions = 0;
+	for (int x = 0; x < w; ++x) {
+		for (int y = 0; y < h; ++y) {
+			Color c = input.GetPixel(x, y);
+			if (!(c == WHITE)) {
+				/* Lookup where this falls in offset */
+				int ox = x % s_offset;
+				int oy = y % s_offset;
+				if (++hits[ox][oy] > mode) {
+					mode = hits[ox][oy];
+					most.first = ox;
+					most.second = oy;
+				}
+				/* Use this offset to hash */
+				Offset o = offset.GetPixel(ox, oy);
+				int hx = (x + o.dx) % s_hash;
+				int hy = (y + o.dy) % s_hash;
+				BUCKET *bucket = &hash[hx * s_hash + hy];
+				if (!bucket->empty()) {
+					++collisions;
+				}
+				bucket->insert(c);
+			}
+		}
+	}
+	return collisions;
+}
+
+static void
 Compress(
 		const Image<Color> &input,
 		Image<bool> &occupancy,
@@ -20,31 +93,53 @@ Compress(
 	occupancy.Allocate(w, h);
 	for (int x = 0; x < w; ++x) {
 		for (int y = 0; y < h; ++y) {
-			bool not_white = !input.GetPixel(x, y).isWhite();
+			bool not_white = !(input.GetPixel(x, y) == WHITE);
 			occupancy.SetPixel(x, y, not_white);
 			p += not_white; // FIXME bad?
 		}
 	}
-
-	/* Construct hash_data + offset */
+	/* Construct offset, perhaps repeatedly */
 	int s_hash, s_offset;
 	s_hash = (int) ceil(sqrt((double) p * 1.01));
 	s_offset = (int) ceil(sqrt((double) p) / 2.0);
-	// TODO use these s's to hash_data and offset
+	BUCKET *colors; std::pair<int, int> max;
+	while (true) {
+		offset.Allocate(s_offset, s_offset);
+		offset.SetAllPixels(ZERO);
+		if (!(s_hash * s_hash < h * w)) {
+			std::cerr << "No perfect hash-function exists!" << std::endl;
+			delete[] colors;
+			return;
+		}
+		/* Attempt to perform a hash using this offset table */
+		colors = new BUCKET[s_hash * s_hash];
+		// TODO use max to incement the offset position
+		/* Collisions will cause us to rehash */
+		if (TryCompress(input, offset, s_offset, s_hash, colors, max)) {
+			delete[] colors;
+			s_offset += 1;
+			s_hash += 1;
+		} else {
+			break;
+		}
+	}
+	/* Set the pixels to their final state */
 	hash_data.Allocate(s_hash, s_hash);
-	offset.Allocate(s_offset, s_offset);
+	hash_data.SetAllPixels(WHITE);
 	for (int x = 0; x < w; ++x) {
 		for (int y = 0; y < h; ++y) {
 			Color c = input.GetPixel(x, y);
-			hash_data.SetPixel(x % s_hash, y % s_hash, c);
-			Offset o(0, 0);
-			offset.SetPixel(x % s_offset, y % s_offset, o);
+			if (!(c == WHITE)) {
+				BUCKET *bucket = &colors[x * s_hash + y];
+				assert(bucket->size() == 1);
+				hash_data.SetPixel(x % s_hash, y % s_hash, *bucket->begin());
+			}
 		}
 	}
+	delete[] colors;
 }
 
-
-void
+static void
 UnCompress(
 		const Image<bool> &occupancy,
 		const Image<Color> &hash_data,
@@ -59,7 +154,6 @@ UnCompress(
 	hh = hash_data.Height();
 	ow = offset.Width();
 	oh = offset.Height();
-
 	/* Set output pixels */
 	output.Allocate(w, h);
 	for (int x = 0; x < w; ++x) {
@@ -73,147 +167,150 @@ UnCompress(
 	}
 }
 
+// ============================================================================
+// ============================================================================
 
-// ===================================================================================================
-// ===================================================================================================
-
-void Compare(const Image<Color> &input1, const Image<Color> &input2, Image<bool> &output) {
-
-  // confirm that the files are the same size
-  if (input1.Width() != input2.Width() ||
-      input1.Height() != input2.Height()) {
-    std::cerr << "Error: can't compare images of different dimensions: " 
-         << input1.Width() << "x" << input1.Height() << " vs " 
-         << input2.Width() << "x" << input2.Height() << std::endl;
-  } else {
-    
-    // confirm that the files are the same size
-    output.Allocate(input1.Width(),input1.Height());
-    int count = 0;
-    for (int i = 0; i < input1.Width(); i++) {
-      for (int j = 0; j < input1.Height(); j++) {
-        Color c1 = input1.GetPixel(i,j);
-        Color c2 = input2.GetPixel(i,j);
-        if (c1.r == c2.r && c1.g == c2.g && c1.b == c2.b)
-          output.SetPixel(i,j,true);
-        else {
-          count++;
-          output.SetPixel(i,j,false);
-        }
-      }      
-    }     
-
-    // confirm that the files are the same size
-    if (count == 0) {
-      std::cout << "The images are identical." << std::endl;
-    } else {
-      std::cout << "The images differ at " << count << " pixel(s)." << std::endl;
-    }
-  }
+static void
+Compare(
+		const Image<Color> &input1,
+		const Image<Color> &input2,
+		Image<bool> &output)
+{
+	int i1w, i1h, i2w, i2h, count = 0;
+	i1w = input1.Width();
+	i1h = input1.Height();
+	i2w = input2.Width();
+	i2h = input2.Height();
+	// confirm that the files are the same size
+	if (i1w != i2w || i1h != i2h) {
+		std::cerr << "Error: can't compare images with different dimensions: "
+			<< i1w << "x" << i1h << " vs " << i2w << "x" << i2h << std::endl;
+	} else {
+		// confirm that the files have the same contents
+		output.Allocate(i1w, i1h);
+		for (int i = 0; i < i1w; i++) {
+			for (int j = 0; j < i1h; j++) {
+				Color c1 = input1.GetPixel(i, j);
+				Color c2 = input2.GetPixel(i, j);
+				bool match = (c1 == c2);
+				count += match; // FIXME bad?
+				output.SetPixel(i, j, match);
+			}
+		}
+		// inform the user of the results
+		std::cout << "The images ";
+		if (count) {
+			std::cout << "differ at " << count << " pixel(s).";
+		} else {
+			std::cout << "are identical.";
+		}
+		std::cout << std::endl;
+	}
 }
 
-// ===================================================================================================
-// ===================================================================================================
-
+// ============================================================================
+// ============================================================================
 
 // to allow visualization of the custom offset image format
-void ConvertOffsetToColor(const Image<Offset> &input, Image<Color> &output) {
-  // prepare the output image to be the same size as the input image
-  output.Allocate(input.Width(),input.Height());
-  for (int i = 0; i < output.Width(); i++) {
-    for (int j = 0; j < output.Height(); j++) {
-      // grab the offset value for this pixel in the image
-      Offset off = input.GetPixel(i,j);
-      // set the pixel in the output image
-      int dx = off.dx;
-      int dy = off.dy;
-      assert (dx >= 0 && dx <= 15);
-      assert (dy >= 0 && dy <= 15);
-      // to make a pretty image with purple, cyan, blue, & white pixels:
-      int red = dx * 16;
-      int green = dy *= 16;
-      int blue = 255;
-      assert (red >= 0 && red <= 255);
-      assert (green >= 0 && green <= 255);
-      output.SetPixel(i,j,Color(red,green,blue));
-    }
-  }
+static void
+ConvertOffsetToColor(const Image<Offset> &input, Image<Color> &output)
+{
+	int iw, ih, r, g, b;
+	iw = input.Width();
+	ih = input.Height();
+	// prepare the output image to be the same size as the input image
+	output.Allocate(iw, ih);
+	for (int i = 0; i < iw; i++) {
+		for (int j = 0; j < ih; j++) {
+			// grab the offset value for this pixel in the image
+			Offset off = input.GetPixel(i, j);
+			// set the pixel in the output image
+			r = off.dx * 16; g = off.dy * 16; b = 255;
+			assert(r >= 0x00 && r <= 0xFF);
+			assert(g >= 0x00 && g <= 0xFF);
+			// to make a pretty image with purple, cyan, blue, & white pixels:
+			output.SetPixel(i, j, Color(r, g, b));
+		}
+	}
 }
 
+// ============================================================================
+// ============================================================================
 
-// ===================================================================================================
-// ===================================================================================================
-
-void usage(char* executable) {
-  std::cerr << "Usage:  4 options" << std::endl;
-  std::cerr << "  1)  " << executable << " compress input.ppm occupancy.pbm data.ppm offset.offset" << std::endl;
-  std::cerr << "  2)  " << executable << " uncompress occupancy.pbm data.ppm offset.offset output.ppm" << std::endl;
-  std::cerr << "  3)  " << executable << " compare input1.ppm input2.ppm output.pbm" << std::endl;
-  std::cerr << "  4)  " << executable << " visualize_offset input.offset output.ppm" << std::endl;
+static void
+usage(char *argv)
+{
+	using std::cerr;
+	cerr << "Four usage options:" << std::endl;
+	cerr << " 1) " << argv << " compress input.ppm occupancy.pbm data.ppm offset.offset\n";
+	cerr << " 2) " << argv << " uncompress occupancy.pbm data.ppm offset.offset output.ppm\n";
+	cerr << " 3) " << argv << " compare input1.ppm input2.ppm output.pbm\n";
+	cerr << " 4) " << argv << " visualize_offset input.offset output.ppm\n";
 }
 
+// ============================================================================
+// ============================================================================
 
-// ===================================================================================================
-// ===================================================================================================
-
-int main(int argc, char* argv[]) {
-  if (argc < 2) { usage(argv[0]); exit(1); }
-
-  if (argv[1] == std::string("compress")) {
-    if (argc != 6) { usage(argv[0]); exit(1); }
-    // the original image:
-    Image<Color> input;
-    // 3 files form the compressed representation:
-    Image<bool> occupancy;
-    Image<Color> hash_data;
-    Image<Offset> offset;
-    input.Load(argv[2]);
-    Compress(input,occupancy,hash_data,offset);
-    // save the compressed representation
-    occupancy.Save(argv[3]);
-    hash_data.Save(argv[4]);
-    offset.Save(argv[5]);
-
-  } else if (argv[1] == std::string("uncompress")) {
-    if (argc != 6) { usage(argv[0]); exit(1); }
-    // the compressed representation:
-    Image<bool> occupancy;
-    Image<Color> hash_data;
-    Image<Offset> offset;
-    occupancy.Load(argv[2]);
-    hash_data.Load(argv[3]);
-    offset.Load(argv[4]);
-    // the reconstructed image
-    Image<Color> output;
-    UnCompress(occupancy,hash_data,offset,output);
-    // save the reconstruction
-    output.Save(argv[5]);
-  
-  } else if (argv[1] == std::string("compare")) {
-    if (argc != 5) { usage(argv[0]); exit(1); }
-    // the original images
-    Image<Color> input1;
-    Image<Color> input2;    
-    input1.Load(argv[2]);
-    input2.Load(argv[3]);
-    // the difference image
-    Image<bool> output;
-    Compare(input1,input2,output);
-    // save the difference
-    output.Save(argv[4]);
-
-  } else if (argv[1] == std::string("visualize_offset")) {
-    if (argc != 4) { usage(argv[0]); exit(1); }
-    // the 8-bit offset image (custom format)
-    Image<Offset> input;
-    input.Load(argv[2]);
-    // a 24-bit color version of the image
-    Image<Color> output;
-    ConvertOffsetToColor(input,output);
-    output.Save(argv[3]);
-
-  } else {
-    usage(argv[0]);
-    exit(1);
-  }
+int
+main(int argc, char *argv[])
+{
+	// The first argument should specify a command
+	if (argc < 2) {
+		usage(argv[0]);
+		return EXIT_FAILURE;
+	}
+	if (argv[1] == std::string("compress")) {
+		if (argc != 6) { usage(argv[0]); exit(1); }
+		// the original image:
+		Image<Color> input;
+		// 3 files form the compressed representation:
+		Image<bool> occupancy;
+		Image<Color> hash_data;
+		Image<Offset> offset;
+		input.Load(argv[2]);
+		Compress(input,occupancy,hash_data,offset);
+		// save the compressed representation
+		occupancy.Save(argv[3]);
+		hash_data.Save(argv[4]);
+		offset.Save(argv[5]);
+	} else if (argv[1] == std::string("uncompress")) {
+		if (argc != 6) { usage(argv[0]); exit(1); }
+		// the compressed representation:
+		Image<bool> occupancy;
+		Image<Color> hash_data;
+		Image<Offset> offset;
+		occupancy.Load(argv[2]);
+		hash_data.Load(argv[3]);
+		offset.Load(argv[4]);
+		// the reconstructed image
+		Image<Color> output;
+		UnCompress(occupancy,hash_data,offset,output);
+		// save the reconstruction
+		output.Save(argv[5]);
+	} else if (argv[1] == std::string("compare")) {
+		if (argc != 5) { usage(argv[0]); exit(1); }
+		// the original images
+		Image<Color> input1;
+		Image<Color> input2;
+		input1.Load(argv[2]);
+		input2.Load(argv[3]);
+		// the difference image
+		Image<bool> output;
+		Compare(input1,input2,output);
+		// save the difference
+		output.Save(argv[4]);
+	} else if (argv[1] == std::string("visualize_offset")) {
+		if (argc != 4) { usage(argv[0]); exit(1); }
+		// the 8-bit offset image (custom format)
+		Image<Offset> input;
+		input.Load(argv[2]);
+		// a 24-bit color version of the image
+		Image<Color> output;
+		ConvertOffsetToColor(input,output);
+		output.Save(argv[3]);
+	} else {
+		usage(argv[0]);
+		return EXIT_FAILURE;
+	}
+	return EXIT_SUCCESS;
 }
